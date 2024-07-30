65ba87573be7569330853fee7b41fde36e49032e
SunYu <sunyu@uml-tech.com>
2024-07-30 16:16:54 +0800

    init

---
+++ src/main/java/sunyu/util/RedisUtil.java
@@ -0,0 +1,137 @@
+package sunyu.util;
+
+import cn.hutool.json.JSONUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.sync.RedisAdvancedClusterCommands;
+
+import java.io.Closeable;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Redis工具类
+ *
+ * @author 孙宇
+ */
+public enum RedisUtil implements Serializable, Closeable {
+    INSTANCE;
+    private Log log = LogFactory.get();
+
+    private Map<String, RedisClient> redisClientMap = new HashMap<>();
+    private Map<String, RedisClusterClient> redisClusterClientMap = new HashMap<>();
+    private Map<String, StatefulRedisConnection<String, String>> redisConnectionMap = new HashMap<>();
+    private Map<String, StatefulRedisClusterConnection<String, String>> redisClusterConnectionMap = new HashMap<>();
+
+
+    /**
+     * 获取standalone命令对象
+     * <pre>
+     * redis://localhost:16379/0
+     * redis://[password@]host[:port][/databaseNumber]
+     * redis://[username:password@]host[:port][/databaseNumber]
+     * rediss://[[username:]password@]host[:port][/database][?[timeout=timeout[d|h|m|s|ms|us|ns]][&clientName=clientName][&libraryName=libraryName][&libraryVersion=libraryVersion]]
+     * redis-socket://[[username:]password@]path[?[timeout=timeout[d|h|m|s|ms|us|ns]][&database=database][&clientName=clientName][&libraryName=libraryName][&libraryVersion=libraryVersion]]
+     * </pre>
+     *
+     * @param uri
+     * @return
+     */
+    public RedisCommands<String, String> standalone(String uri) {
+        RedisClient client = RedisClient.create(uri);
+        StatefulRedisConnection<String, String> conn = client.connect();
+        log.debug("已连接到redis standalone {}", uri);
+        redisClientMap.put(uri, client);
+        redisConnectionMap.put(uri, conn);
+        RedisCommands<String, String> commands = conn.sync();
+        return commands;
+    }
+
+
+    /**
+     * 获取sentinel命令对象
+     * <pre>
+     * redis-sentinel://localhost:26379,localhost:26380/0#mymaster
+     * redis-sentinel://[password@]host[:port][,host2[:port2]][/databaseNumber]#sentinelMasterId
+     * </pre>
+     *
+     * @param uri
+     * @return
+     */
+    public RedisCommands<String, String> sentinel(String uri) {
+        RedisClient client = RedisClient.create(uri);
+        StatefulRedisConnection<String, String> conn = client.connect();
+        log.debug("已连接到redis sentinel {}", uri);
+        redisClientMap.put(uri, client);
+        redisConnectionMap.put(uri, conn);
+        RedisCommands<String, String> commands = conn.sync();
+        return commands;
+    }
+
+    /**
+     * 获取cluster命令对象
+     * <pre>
+     * redis://[password@]host[:port]
+     * redis://[username:password@]host[:port]
+     * </pre>
+     *
+     * @param uris
+     * @return
+     */
+    public RedisAdvancedClusterCommands<String, String> cluster(List<String> uris) {
+        List<RedisURI> uriList = new ArrayList();
+        for (String uri : uris) {
+            uriList.add(RedisURI.create(uri));
+        }
+        RedisClusterClient client = RedisClusterClient.create(uriList);
+        StatefulRedisClusterConnection<String, String> conn = client.connect();
+        String urisString = JSONUtil.toJsonStr(uris);
+        log.debug("已连接到redis cluster {}", urisString);
+        redisClusterClientMap.put(urisString, client);
+        redisClusterConnectionMap.put(urisString, conn);
+        RedisAdvancedClusterCommands<String, String> commands = conn.sync();
+        return commands;
+    }
+
+
+    /**
+     * 构建工具类
+     *
+     * @return
+     */
+    public RedisUtil build() {
+        return INSTANCE;
+    }
+
+    /**
+     * 回收资源
+     */
+    @Override
+    public void close() {
+        redisConnectionMap.forEach((uri, redisConnection) -> {
+            log.debug("关闭链接 {}", uri);
+            redisConnection.close();
+        });
+        redisClientMap.forEach((uri, redisClient) -> {
+            log.debug("关闭客户端", uri);
+            redisClient.shutdown();
+        });
+        redisClusterConnectionMap.forEach((uri, redisClusterConnection) -> {
+            log.debug("关闭链接 {}", uri);
+            redisClusterConnection.close();
+        });
+        redisClusterClientMap.forEach((uri, redisClusterClient) -> {
+            log.debug("关闭客户端", uri);
+            redisClusterClient.shutdown();
+        });
+    }
+}
---
+++ src/test/java/sunyu/util/test/TestRedisUtil.java
@@ -0,0 +1,99 @@
+package sunyu.util.test;
+
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import io.lettuce.core.KeyValue;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.sync.RedisAdvancedClusterCommands;
+import org.junit.jupiter.api.Test;
+import sunyu.util.RedisUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestRedisUtil {
+    Log log = LogFactory.get();
+
+    @Test
+    void t001() {
+        // Syntax: redis://[password@]host[:port][/databaseNumber]
+        // Syntax: redis://[username:password@]host[:port][/databaseNumber]
+        RedisClient redisClient = RedisClient.create("redis://192.168.11.39:16379/0");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        log.debug("Connected to Redis");
+        RedisCommands<String, String> redisCommands = connection.sync();
+
+        log.debug(redisCommands.get("subsidy:bc:userinfo"));
+
+        connection.close();
+        redisClient.shutdown();
+    }
+
+    @Test
+    void t002() {
+        // Syntax: redis://[password@]host[:port]
+        // Syntax: redis://[username:password@]host[:port]
+        List<RedisURI> uris = new ArrayList();
+        uris.add(RedisURI.create("redis://192.168.11.124:7001"));
+        uris.add(RedisURI.create("redis://192.168.11.124:7002"));
+        uris.add(RedisURI.create("redis://192.168.11.124:7003"));
+        uris.add(RedisURI.create("redis://192.168.11.125:7004"));
+        uris.add(RedisURI.create("redis://192.168.11.125:7005"));
+        uris.add(RedisURI.create("redis://192.168.11.125:7006"));
+        RedisClusterClient redisClient = RedisClusterClient.create(uris);
+        StatefulRedisClusterConnection<String, String> connection = redisClient.connect();
+        log.debug("Connected to Redis Cluster");
+        RedisAdvancedClusterCommands<String, String> redisClusterCommands = connection.sync();
+
+        for (KeyValue<String, String> kv : redisClusterCommands.mget("relation:16200442", "farm:realtime:0865306056453850")) {
+            log.debug("{} {}", kv.getKey(), kv.getValue());
+        }
+
+        connection.close();
+        redisClient.shutdown();
+    }
+
+    @Test
+    void t003() {
+        // Syntax: redis-sentinel://[password@]host[:port][,host2[:port2]][/databaseNumber]#sentinelMasterId
+        RedisClient redisClient = RedisClient.create("redis-sentinel://localhost:26379,localhost:26380/0#mymaster");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        log.debug("Connected to Redis using Redis Sentinel");
+        RedisCommands<String, String> redisSentinelCommands = connection.sync();
+
+        connection.close();
+        redisClient.shutdown();
+    }
+
+
+    @Test
+    void t004() {
+        RedisUtil redisUtil = RedisUtil.INSTANCE.build();//全局只需要一个
+        RedisCommands<String, String> standalone = redisUtil.standalone("redis://192.168.11.39:16379/0");//全局只需要一个
+
+        log.debug(standalone.get("subsidy:bc:userinfo"));
+
+        redisUtil.close();//如果程序不再使用了，可以调用这个
+    }
+
+    @Test
+    void t005() {
+        RedisUtil redisUtil = RedisUtil.INSTANCE.build();//全局只需要一个
+        RedisAdvancedClusterCommands<String, String> cluster = redisUtil.cluster(
+                Arrays.asList("redis://192.168.11.124:7001", "redis://192.168.11.124:7002", "redis://192.168.11.124:7003",
+                        "redis://192.168.11.125:7004", "redis://192.168.11.125:7005", "redis://192.168.11.125:7006"));//全局只需要一个
+
+        for (KeyValue<String, String> kv : cluster.mget("relation:16200442", "farm:realtime:0865306056453850")) {
+            log.debug("{} {}", kv.getKey(), kv.getValue());
+        }
+
+        redisUtil.close();//如果程序不再使用了，可以调用这个
+    }
+
+}
